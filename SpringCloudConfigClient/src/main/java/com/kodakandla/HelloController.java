99902c8f529d61eb9fd75f3c159253207e886eab
rohithkk <rohithkodakandla@gmail.com>
2019-03-04 22:04:53 -0500

    committing eureka server project

--- SpringCloudConfigClient/src/main/java/com/kodakandla/HelloController.java
+++ SpringCloudConfigClient/src/main/java/com/kodakandla/HelloController.java
@@ -3,16 +3,30 @@ package com.kodakandla;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.discovery.EurekaClient;
+
 @RestController
 public class HelloController {
 
+	@Autowired
+	@Lazy
+	private EurekaClient eurekaClient;
+	
+	@GetMapping(value="/hello")
+	public String hello() {
+		return String.format("Hello from '%s'!", eurekaClient.getApplication(appName).getName());
+	}
+	
 	@RequestMapping( value = "/getProps",  method = RequestMethod.GET)
 	public ResponseEntity<?> whoami() {
 		
@@ -32,4 +46,7 @@ public class HelloController {
 	
 	@Value("${test.prop.three}")
 	private String propThree;
+	
+	@Value("${spring.application.name}")
+	private String appName;
 }
