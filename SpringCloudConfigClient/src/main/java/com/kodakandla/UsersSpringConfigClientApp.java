557061002b76bb6776f686c254f244abe50e4524
rohithkk <rohithkodakandla@gmail.com>
2019-03-08 19:33:32 -0500

    renaming project

--- SpringCloudConfigClient/src/main/java/com/kodakandla/UsersSpringConfigClientApp.java
+++ SpringCloudConfigClient/src/main/java/com/kodakandla/UsersSpringConfigClientApp.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConfigClientApp {
+public class UsersSpringConfigClientApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigClientApp.class, args);
+		SpringApplication.run(UsersSpringConfigClientApp.class, args);
 	}
 }
