250aae1e3c3acb8635724e193be7484defad3380
rohithkk <rohithkodakandla@gmail.com>
2019-03-09 19:11:03 -0500

    sc

--- Users-SpringCloudConfigClient/src/main/java/com/kodakandla/SecurityConfig.java
+++ Users-SpringCloudConfigClient/src/main/java/com/kodakandla/SecurityConfig.java
@@ -18,7 +18,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
+		http
+			.csrf().disable();
+		
 		http
 			.authorizeRequests()
 				.requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
